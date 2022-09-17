4b0f40f98142a20c0af6673c42a6dd159d6c6247
monu <monuchahar01@gmail.com>
2022-09-17 22:32:47 +0530

    For testing

---
+++ tests/unit/test_utils.py
@@ -0,0 +1,12 @@
+import pytest
+from deepClassifier.utils import read_yaml
+from pathlib import Path
+from box import ConfigBox
+
+
+yaml_files=[
+    "tests/data/empty.yaml",
+    "test/data/demo.yaml"
+
+
+]
